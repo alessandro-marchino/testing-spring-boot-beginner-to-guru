5af4f218b9f03e818c851aff96ca43b258dba020
amarchino <marchino.alessandro@gmail.com>
2025-11-24 21:01:20 +0100

    Lesson 24

--- tdd-by-example/src/main/java/guru/springframework/Money.java
+++ tdd-by-example/src/main/java/guru/springframework/Money.java
@@ -2,7 +2,16 @@ package guru.springframework;
 
 public abstract class Money {
 
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
     int amount;
+
+    public abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object obj) {
