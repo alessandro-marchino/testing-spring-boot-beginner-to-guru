507f7f38a80885bfae21699ed61b83d679e124df
amarchino <marchino.alessandro@gmail.com>
2025-11-26 23:41:24 +0100

    Lesson 38

---
+++ getting-started-junit5/src/test/java/guru/springframework/GreatingTest.java
@@ -0,0 +1,18 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+class GreatingTest {
+
+    @Test
+    void helloWorld() {
+        Greeting greeting = new Greeting();
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void helloWorld1() {
+        Greeting greeting = new Greeting();
+        System.out.println(greeting.helloWorld("John"));
+    }
+}
