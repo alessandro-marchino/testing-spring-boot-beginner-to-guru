dc28308fe101f73a96c254490ff6a0945f224f61
amarchino <marchino.alessandro@gmail.com>
2025-11-28 23:42:38 +0100

    Lesson 40

--- getting-started-junit5/src/test/java/guru/springframework/GreatingTest.java
+++ getting-started-junit5/src/test/java/guru/springframework/GreatingTest.java
@@ -1,14 +1,31 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreatingTest {
     private Greeting greeting;
 
+    @BeforeAll
+    static void beforeClass() {
+        System.out.println("Before - I am only called once!");
+    }
+    @AfterAll
+    static void afterClass() {
+        System.out.println("After - I am only called once!");
+    }
+
     @BeforeEach
     void setUp() {
         greeting = new Greeting();
+        System.out.println("In before each...");
+    }
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each...");
     }
 
     @Test
