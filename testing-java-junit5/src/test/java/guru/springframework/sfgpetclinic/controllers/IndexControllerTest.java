072d023479346508370fbd349eb139cabeb4d12b
amarchino <marchino.alessandro@gmail.com>
2025-12-02 21:25:40 +0100

    Lesson 48

---
+++ testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong view returned");
+        assertEquals("index", controller.index(), () -> "Another expensive message - Make me only if you need to");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive message to build for my test");
+    }
+}
