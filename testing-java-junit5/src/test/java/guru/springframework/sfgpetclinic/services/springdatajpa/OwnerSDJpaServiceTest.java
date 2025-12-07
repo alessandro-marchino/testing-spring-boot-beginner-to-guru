36f00a3221d7ba5d734a5bd66374ec095a554197
amarchino <marchino.alessandro@gmail.com>
2025-12-07 17:37:56 +0100

    Lesson 51

---
+++ testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,53 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+
+@Disabled("Disabled until we learn Mocking")
+class OwnerSDJpaServiceTest {
+    OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Test
+    void testDelete() {
+        
+    }
+
+    @Test
+    void testDeleteById() {
+        
+    }
+
+    @Test
+    void testFindAll() {
+        
+    }
+
+    @Test
+    void testFindAllByLastNameLike() {
+        
+    }
+
+    @Test
+    void testFindById() {
+        
+    }
+
+    @Test
+    @Disabled
+    void testFindByLastName() {
+        Owner owner = service.findByLastName("Buck");
+    }
+
+    @Test
+    void testSave() {
+        
+    }
+}
