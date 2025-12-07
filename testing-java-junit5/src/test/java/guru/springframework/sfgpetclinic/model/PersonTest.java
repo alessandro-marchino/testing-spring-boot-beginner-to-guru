725bedc433d0f3bf9a839fd3b8836358a0216e0d
amarchino <marchino.alessandro@gmail.com>
2025-12-07 17:24:27 +0100

    Lesson 49

---
+++ testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+    @Test
+    void groupedAssertions() {
+        Person person = new Person(1L, "Joe", "Buck");
+        assertAll("Test Props set",
+            () -> assertEquals("Joe", person.getFirstName()),
+            () -> assertEquals("Buck", person.getLastName()),
+            () -> assertEquals(Long.valueOf(1L), person.getId())
+        );
+    }
+
+    @Test
+    void groupedAssertionsMsg() {
+        Person person = new Person(1L, "Joe", "Buck");
+        assertAll("Test Props set",
+            () -> assertEquals("Joe", person.getFirstName(), "First Name failed"),
+            () -> assertEquals("Buck", person.getLastName(), "Last Name failed"),
+            () -> assertEquals(Long.valueOf(1L), person.getId(), "Id failed")
+        );
+    }
+}
