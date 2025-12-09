6e19953c7efbd6fcbc63b67a0419520df989fd4a
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-12-09 08:31:31 +0100

    Lesson 54

--- testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -2,8 +2,13 @@ package guru.springframework.sfgpetclinic.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -28,5 +33,23 @@ class IndexControllerTest {
     @DisplayName("Test exception")
     void oupsHandler() {
         assertThrows(ValueNotFoundException.class, () -> controller.oopsHandler());
+    }
+
+    @Test
+    @Disabled
+    void testTimeout() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(2000);
+            System.out.println("I got here");
+        });
+    }
+
+    @Test
+    @Disabled
+    void testTimeoutPreemptively() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(2000);
+            System.out.println("I got here too");
+        });
     }
 }
