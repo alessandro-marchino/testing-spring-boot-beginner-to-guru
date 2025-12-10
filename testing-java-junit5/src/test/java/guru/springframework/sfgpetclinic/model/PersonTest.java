5b4609df98d5c92e084d3d6c57d32396361846d9
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-12-10 08:56:04 +0100

    Lesson 70

--- testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -4,12 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 import guru.springframework.ModelTests;
+import guru.springframework.sfgpetclinic.RepeatedTests;
 
 class PersonTest implements ModelTests {
     @Test
@@ -32,14 +34,18 @@ class PersonTest implements ModelTests {
         );
     }
 
-    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition}/{totalRepetitions}")
-    @DisplayName("My Repeated Test")
-    void myRepeatedTest() {
-        // TODO
-    }
-
-    @RepeatedTest(5)
-    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
-        System.out.println(testInfo.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    @Nested
+    class PersonRepeatedTest implements RepeatedTests {
+        
+        @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition}/{totalRepetitions}")
+        @DisplayName("My Repeated Test")
+        void myRepeatedTest() {
+            // TODO
+        }
+    
+        @RepeatedTest(5)
+        void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+            System.out.println(testInfo.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+        }
     }
 }
