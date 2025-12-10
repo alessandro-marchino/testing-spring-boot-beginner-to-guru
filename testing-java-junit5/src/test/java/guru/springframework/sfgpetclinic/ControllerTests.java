253517bcfbe6875854627bf4b7fe52ca3426aa9f
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-12-10 08:40:04 +0100

    Lesson 66

--- testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 @Tag("Controller")
+@TestInstance(Lifecycle.PER_CLASS)
 public interface ControllerTests {
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Let's do something here");
+    }
 
 }
