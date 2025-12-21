50fe9476df9f4ab9a0d66e440950930e601aed5e
amarchino <marchino.alessandro@gmail.com>
2025-12-21 19:50:34 +0100

    Lesson 150

---
+++ tb2g-testing-spring-master/src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterYanniTest.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.YanniConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(classes = { BaseConfig.class, YanniConfig.class })
+class HearingInterpreterYanniTest {
+
+    @Autowired HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIHeard() {
+        String word = hearingInterpreter.whatIHeard();
+        assertEquals("Yanni", word);
+    }
+}
