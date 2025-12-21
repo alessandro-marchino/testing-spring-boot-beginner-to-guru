8908da33a414c89c6c0855d089ea75564838f018
amarchino <marchino.alessandro@gmail.com>
2025-12-21 20:54:09 +0100

    Lesson 156

---
+++ tb2g-testing-spring-master/src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterLaurelPropertiesTest.java
@@ -0,0 +1,34 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@TestPropertySource("classpath:laurel.properties")
+@ActiveProfiles("laurel-properties")
+@SpringJUnitConfig(classes = HearingInterpreterLaurelPropertiesTest.TestConfig.class)
+class HearingInterpreterLaurelPropertiesTest {
+
+    @Configuration
+    @Profile("laurel-properties")
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig {
+        // Empty
+    }
+
+    @Autowired HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIHeard() {
+        String word = hearingInterpreter.whatIHeard();
+        assertEquals("LaUrEl", word);
+    }
+}
