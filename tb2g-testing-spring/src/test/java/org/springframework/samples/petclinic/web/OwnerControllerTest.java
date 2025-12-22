b5d531a772e8c3bbea7f6577578c62bd5189d409
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-12-22 11:38:13 +0100

    Lesson 163

--- tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,20 +1,44 @@
 package org.springframework.samples.petclinic.web;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringJUnitWebConfig(locations = { "classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml" })
 class OwnerControllerTest {
     @Autowired
     OwnerController ownerController;
-
     @Autowired
     ClinicService clinicService;
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
 
     @Test
     void tempTest() {
-        // Empty
+        // Context loads
+    }
+
+    @Test
+    void initCreationForm() throws Exception {
+        // Given
+        // When
+        mockMvc.perform(get("/owners/new"))
+        // Then
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("owner"))
+            .andExpect(view().name("owners/createOrUpdateOwnerForm"));
     }
 }
