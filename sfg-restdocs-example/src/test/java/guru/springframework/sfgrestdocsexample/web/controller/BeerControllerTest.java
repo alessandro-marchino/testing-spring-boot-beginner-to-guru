4c7e4a50fed1e39380d105b856bc6349e804bd8f
amarchino <marchino.alessandro@gmail.com>
2025-12-24 23:19:16 +0100

    Lesson 202

--- sfg-restdocs-example/src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ sfg-restdocs-example/src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -22,7 +22,10 @@ import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RestDocumentationExtension.class)
@@ -44,8 +47,12 @@ class BeerControllerTest {
     void getBeerById() throws Exception {
         given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andDo(document("v1/beer", pathParameters(
+                parameterWithName("beerId").description("UUID of desired beer to get")
+            )));
     }
 
     @Test
@@ -56,7 +63,7 @@ class BeerControllerTest {
         mockMvc.perform(post("/api/v1/beer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
-                .andExpect(status().isCreated());
+            .andExpect(status().isCreated());
     }
 
     @Test
@@ -64,19 +71,19 @@ class BeerControllerTest {
         BeerDto beerDto =  getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
-        mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
+        mockMvc.perform(put("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
-                .andExpect(status().isNoContent());
+            .andExpect(status().isNoContent());
     }
 
     BeerDto getValidBeerDto(){
         return BeerDto.builder()
-                .beerName("Nice Ale")
-                .beerStyle(BeerStyleEnum.ALE)
-                .price(new BigDecimal("9.99"))
-                .upc(123123123123L)
-                .build();
+            .beerName("Nice Ale")
+            .beerStyle(BeerStyleEnum.ALE)
+            .price(new BigDecimal("9.99"))
+            .upc(123123123123L)
+            .build();
 
     }
 
